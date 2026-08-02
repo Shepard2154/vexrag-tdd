da6ce3e016caed9c3326dda68d92f1d96cfe7038
developer <john-shepard-2154@yandex.ru>
2026-08-02 09:41:27 +0300

    feat: CLI prints ready when invoked

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,13 @@
 import subprocess
+import sys
 
 
 def test_vexrag_invokation():
     invokation_result: subprocess.CompletedProcess = subprocess.run(
-        "vexrag",
-        shell=True,
+        [sys.executable, "-m", "vexrag",],
         capture_output=True,
+        text=True,
     )
     assert invokation_result.returncode == 0
+    assert 'ready' in invokation_result.stdout.lower()
 
